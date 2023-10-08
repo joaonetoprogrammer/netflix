29eded3b3d940c5bd9a0b773d30ad18fbf3526c2
joaonetoprogrammer <joaonetoprogrammer@gmail.com>
2023-10-08 01:13:59 -0300

    page_filmes_completed layout

--- filme/views.py
+++ filme/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
@@ -21,3 +22,16 @@ class HomeFilmes(ListView):
 class DetalhesFilme(DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
+
+    def get(self, request, *args, **kwargs):
+        filme = self.get_object()
+        filme.visualizacoes +=1
+        filme.save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DetalhesFilme, self).get_context_data(**kwargs)
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)
+        context['filmes_relacionados'] = filmes_relacionados
+        return context
